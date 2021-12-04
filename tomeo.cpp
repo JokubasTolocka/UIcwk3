4a4decb1b3b6208d12abee00d3b0c6d540c6dd81
Jokūbas Toločka <jokubas.tj@gmail.com>
2021-12-04 18:57:47 +0200

    prep for iteration 2, search plus scroll

--- tomeo.cpp
+++ tomeo.cpp
@@ -9,13 +9,16 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <QApplication>
 #include <QtMultimediaWidgets/QVideoWidget>
 #include <QMediaPlaylist>
 #include <string>
+#include <sstream>
 #include <vector>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QHBoxLayout>
+#include <QTextEdit>
 #include <QtCore/QFileInfo>
 #include <QtWidgets/QFileIconProvider>
 #include <QDesktopServices>
@@ -25,6 +28,10 @@
 #include <QtCore/QDirIterator>
 #include "the_player.h"
 #include "the_button.h"
+using std::cout; using std::cerr;
+using std::endl; using std::string;
+using std::ifstream; using std::ostringstream;
+using std::istringstream;
 
 // read in videos and thumbnails to this directory
 std::vector<TheButtonInfo> getInfoIn (std::string loc) {
@@ -37,7 +44,7 @@ std::vector<TheButtonInfo> getInfoIn (std::string loc) {
 
         QString f = it.next();
 
-            if (f.contains("."))
+        if (f.contains("."))
 
 #if defined(_WIN32)
             if (f.contains(".wmv"))  { // windows
@@ -45,10 +52,10 @@ std::vector<TheButtonInfo> getInfoIn (std::string loc) {
             if (f.contains(".mp4") || f.contains("MOV"))  { // mac/linux
 #endif
 
-            QString thumb = f.left( f .length() - 4) +".png";
-            if (QFile(thumb).exists()) { // if a png thumbnail exists
-                // Adding this comment to check the Pull Request feature
-                QImageReader *imageReader = new QImageReader(thumb);
+                QString thumb = f.left( f .length() - 4) +".png";
+                if (QFile(thumb).exists()) { // if a png thumbnail exists
+                    // Adding this comment to check the Pull Request feature
+                    QImageReader *imageReader = new QImageReader(thumb);
                     QImage sprite = imageReader->read(); // read the thumbnail
                     if (!sprite.isNull()) {
                         QIcon* ico = new QIcon(QPixmap::fromImage(sprite)); // voodoo to create an icon for the button
@@ -57,13 +64,23 @@ std::vector<TheButtonInfo> getInfoIn (std::string loc) {
                     }
                     else
                         qDebug() << "warning: skipping video because I couldn't process thumbnail " << thumb << endl;
+                }
+                else
+                    qDebug() << "warning: skipping video because I couldn't find thumbnail " << thumb << endl;
             }
-            else
-                qDebug() << "warning: skipping video because I couldn't find thumbnail " << thumb << endl;
-        }
     }
 
     return out;
+}
+
+string readFileIntoString(const string& path) {
+    ifstream input_file(path);
+    if (!input_file.is_open()) {
+        cerr << "Could not open the file - '"
+             << path << "'" << endl;
+        exit(EXIT_FAILURE);
+    }
+    return string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
 }
 
 
@@ -74,6 +91,26 @@ int main(int argc, char *argv[]) {
 
     // create the Qt Application
     QApplication app(argc, argv);
+
+    string filename("C:\\Users\\miada\\OneDrive\\Documents\\Uni Work\\Year2\\Sem1\\UserInterfaces2811\\metadata.csv");
+    string file_contents;
+
+    //    std::map<int, std::vector<string>> csv_contents;
+    //    char delimiter = ',';
+
+    //    file_contents = readFileIntoString(filename);
+    //    cout << "file contents: " << file_contents << endl;
+
+        //convert to Qstring for use in TextEdit
+    //    QString qstr = QString::fromStdString(file_contents);
+
+    //    istringstream sstream(file_contents);
+    //    //vector of strings
+    //    std::vector<string> items;
+    //    string line1 = "gopro cycling in the alps, mountains";
+    //    string line2 = "panda sanctuary in china, beijing";
+    //    items.push_back(line1);
+    //    items.push_back(line2);
 
     // collect all the videos in the folder
     std::vector<TheButtonInfo> videos;
@@ -93,13 +130,18 @@ int main(int argc, char *argv[]) {
         switch( result )
         {
         case QMessageBox::Yes:
-          QDesktopServices::openUrl(QUrl("https://leeds365-my.sharepoint.com/:u:/g/personal/scstke_leeds_ac_uk/EcGntcL-K3JOiaZF4T_uaA4BHn6USbq2E55kF_BTfdpPag?e=n1qfuN"));
-          break;
+            QDesktopServices::openUrl(QUrl("https://leeds365-my.sharepoint.com/:u:/g/personal/scstke_leeds_ac_uk/EcGntcL-K3JOiaZF4T_uaA4BHn6USbq2E55kF_BTfdpPag?e=n1qfuN"));
+            break;
         default:
             break;
         }
         exit(-1);
     }
+
+    //widget for text beneath player
+    QTextEdit *description = new QTextEdit;
+    //    description->setText(qstr);
+    description->setReadOnly(true);
 
     // the widget that will show the video
     QVideoWidget *videoWidget = new QVideoWidget;
@@ -112,33 +154,39 @@ int main(int argc, char *argv[]) {
     QWidget *buttonWidget = new QWidget();
     // a list of the buttons
     std::vector<TheButton*> buttons;
-    // the buttons are arranged horizontally
-    QHBoxLayout *layout = new QHBoxLayout();
-    buttonWidget->setLayout(layout);
+    // the buttons are arranged **vertically
+    QVBoxLayout *thumbnailLayout = new QVBoxLayout();
+    buttonWidget->setLayout(thumbnailLayout);
 
 
     // create the four buttons
-    for ( int i = 0; i < 4; i++ ) {
+    for ( int i = 0; i < 6; i++ ) {
         TheButton *button = new TheButton(buttonWidget);
         button->connect(button, SIGNAL(jumpTo(TheButtonInfo* )), player, SLOT (jumpTo(TheButtonInfo*))); // when clicked, tell the player to play.
         buttons.push_back(button);
-        layout->addWidget(button);
+        thumbnailLayout->addWidget(button);
         button->init(&videos.at(i));
     }
+
+    //right hand side, player and text
+    QVBoxLayout *playerLayout = new QVBoxLayout();
+    playerLayout->addWidget(videoWidget);
+    playerLayout->addWidget(description);
 
     // tell the player what buttons and videos are available
     player->setContent(&buttons, & videos);
 
     // create the main window and layout
     QWidget window;
-    QVBoxLayout *top = new QVBoxLayout();
-    window.setLayout(top);
+    QHBoxLayout *view = new QHBoxLayout();
     window.setWindowTitle("tomeo");
     window.setMinimumSize(800, 680);
 
     // add the video and the buttons to the top level widget
-    top->addWidget(videoWidget);
-    top->addWidget(buttonWidget);
+    view->addWidget(buttonWidget);
+    view->addLayout(playerLayout);
+
+    window.setLayout(view);
 
     // showtime!
     window.show();
